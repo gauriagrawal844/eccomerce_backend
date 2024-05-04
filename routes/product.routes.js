09d47600b0f723d97df42e51548edda227c75afd
Gauri Agrawal <agrawalgauri51@gmail.com>
2024-05-05 02:34:37 +0530

    Multer

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,7 +1,8 @@
 const ProductController = require("../controllers/product.controller");
+const upload = require('../utils/cloudinaryConfig');
 const router = require("express").Router();
 
-router.post("/create", ProductController.create);
+router.post("/create",upload.single('image'), ProductController.create);
 router.get("/all", ProductController.all);
 router.get("/single", ProductController.get);
 router.delete("/delete", ProductController.delete);
